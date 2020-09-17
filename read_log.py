f1d7d7b71e8aab129b45c94869c531074693e4d6
kniewierski <kniewierski@gmail.com>
2020-09-17 17:51:09 -0500

    test for year

--- read_log.py
+++ read_log.py
@@ -5,7 +5,4 @@ from collections import Counter
 for line in open(FILE_NAME):
   repl = line.replace("[", "#", 10).replace(":", "#", 10).replace("/", "#", 10)
   year = repl.split('#')
-  last = sum(1 for i in year if i == "1995")
-  print("There were ", last, "total requests made in the last year.")
-  total = sum(1 for i in year)
-  print("There were ", total, "total requests made in the time period represented by the log.")
+  print(year)
