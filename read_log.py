1690fd1c572ca08bb49a47ad00dc3c68a737e2f0
kniewierski <kniewierski@gmail.com>
2020-09-17 17:27:59 -0500

    changed spl to year

--- read_log.py
+++ read_log.py
@@ -4,8 +4,8 @@ from collections import Counter
 
 for line in open(FILE_NAME):
   repl = line.replace("[", "#", 10).replace(":", "#", 10).replace("/", "#", 10)
-  spl = repl.split('#')
-  last = sum(1 for i in spl if i == "1995")
+  year = repl.split('#')
+  last = sum(1 for i in year if i == "1995")
   print("There were ", last, "total requests made in the last year.")
-  total = sum(1 for i in spl)
+  total = sum(1 for i in year)
   print("There were ", total, "total requests made in the time period represented by the log.")
