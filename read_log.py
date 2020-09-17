4e3165fed7b99822d739fbe6a88c8f7a9321ea82
kniewierski <kniewierski@gmail.com>
2020-09-17 16:18:42 -0500

    fixed error in line.split

--- read_log.py
+++ read_log.py
@@ -4,5 +4,5 @@ for line in open(FILE_NAME):
   line.replace("[", "#", 10)
   line.replace("/", "#", 10)
   line.replace("]", "#", 10)
-  year = line.split('#',str)
+  year = line.split('#')
   print(year)
