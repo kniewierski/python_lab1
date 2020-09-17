43123eac26e42d2e1b007632062afbe3e7792002
kniewierski <kniewierski@gmail.com>
2020-09-17 17:16:04 -0500

    created counter

--- read_log.py
+++ read_log.py
@@ -1,6 +1,18 @@
 FILE_NAME = './local_copy.log'
 
+from collections import Counter
+
 for line in open(FILE_NAME):
   repl = line.replace("[", "#", 10).replace(":", "#", 10).replace("/", "#", 10)
-  print(repl.split('#'))
-  
+  spl = repl.split('#')
+
+  counter = 0
+    for spl[3]="1995"
+        counter = counter + 1
+        print("There were ", counter, "total requests made in the last year.")
+    for spl
+        counter = counter + 1
+        print("There were " counter, "total requests made in the time period represented by the log.")
+
+    
+        
