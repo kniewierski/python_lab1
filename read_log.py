732b01c37deaa2c7196667c50b07953782c5cb9a
kniewierski <kniewierski@gmail.com>
2020-09-17 16:48:37 -0500

    fixed repl

--- read_log.py
+++ read_log.py
@@ -1,6 +1,6 @@
 FILE_NAME = './local_copy.log'
 
 for line in open(FILE_NAME):
-  repl = line.replace("[", "#", 10).replace(":", "#", 10).replace("]", "#", 10)
+  repl = line.replace("[", "#", 10).replace(":", "#", 10).replace("/", "#", 10)
   print(repl.split('#'))
   
