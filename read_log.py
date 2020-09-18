1930551d286bc53716cadb67883bbb225a140ab1
kniewierski <kniewierski@gmail.com>
2020-09-17 19:33:59 -0500

    fix test loop

--- read_log.py
+++ read_log.py
@@ -8,9 +8,9 @@ total = 0
 for line in open(FILE_NAME):
   repl = line.replace("[", "#", 10).replace(":", "#", 10).replace("/", "#", 10)
   year = repl.split('#')
-  last = sum(1 for i in year if i == "1995")
-  total = sum(1 for i in year)
-  for i in year:
+  last = sum(1 for i in year[3] if i == "1995")
+  total = sum(1 for i in year[3])
+  for i in year[3]:
     print(i)
 print("There were ", last, "total requests made in the last year.")
 print("There were ", total, "total requests made in the time period represented by the log.")
