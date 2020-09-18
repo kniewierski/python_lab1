bfc69b359a7c3b81d42502a1661fa29dea2c6217
kniewierski <kniewierski@gmail.com>
2020-09-17 19:21:21 -0500

    moved print outside of for loop

--- read_log.py
+++ read_log.py
@@ -5,4 +5,8 @@ from collections import Counter
 for line in open(FILE_NAME):
   repl = line.replace("[", "#", 10).replace(":", "#", 10).replace("/", "#", 10)
   year = repl.split('#')
-  print(year[3])
+  last = sum(1 for i in year if i == "1995")
+  total = sum(1 for i in year)
+  
+print("There were ", last, "total requests made in the last year.")
+print("There were ", total, "total requests made in the time period represented by the log.")
