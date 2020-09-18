c0ee663e25d3ecbbf22dc4cd17229237e38de806
kniewierski <kniewierski@gmail.com>
2020-09-17 20:04:10 -0500

    fix if statement indent

--- read_log.py
+++ read_log.py
@@ -11,7 +11,7 @@ for line in open(FILE_NAME):
   total += 1
   if len(year)>=3:
     if year[3] == str(1995):
-    last += 1
+        last += 1
 
 print("There were ", last, "total requests made in the last year.")
 print("There were ", total, "total requests made in the time period represented by the log.")
