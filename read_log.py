3f7edc2a27847f3b87a72d2857a06eed68b783cc
kniewierski <kniewierski@gmail.com>
2020-09-22 13:56:48 -0500

    fixed variables to count total requests per day of week

--- read_log.py
+++ read_log.py
@@ -4,10 +4,16 @@ import re
 from datetime import date
 from datetime import datetime
 
-ERRORS = []
-
 last = 0
 total = 0
+mondays = 0
+tuesdays = 0
+wednesdays = 0
+thursdays = 0
+fridays = 0
+saturdays = 0
+sundays = 0
+
 regex = re.compile(r".*\[([^:]*:.*) \-[0-9]{4}\] \"([A-Z]+) (.+?) (HTTP.*\"|\") ([2-5]0[0-9]) .*")
 
 for line in open(FILE_NAME):
@@ -21,44 +27,20 @@ for line in open(FILE_NAME):
   if len(elements) < 3:
     continue
   date = datetime.strptime(elements[1], "%d/%b/%Y:%H:%M:%S")
-
-  mon1 = datetime.strptime("09/Oct/1995:00:00:00", "%d/%b/%Y:%H:%M:%S")
-  tue1 = datetime.strptime("10/Oct/1995:00:00:00", "%d/%b/%Y:%H:%M:%S")
-  wed1 = datetime.strptime("11/Oct/1995:00:00:00", "%d/%b/%Y:%H:%M:%S")
-  thu1 = datetime.strptime("05/Oct/1995:00:00:00", "%d/%b/%Y:%H:%M:%S")
-  fri1 = datetime.strptime("06/Oct/1995:00:00:00", "%d/%b/%Y:%H:%M:%S")
-  sat1 = datetime.strptime("07/Oct/1995:00:00:00", "%d/%b/%Y:%H:%M:%S")
-  sun1 = datetime.strptime("08/Oct/1995:00:00:00", "%d/%b/%Y:%H:%M:%S")
-
-  mondays = 1
-  tuesdays = 1
-  wednesdays = 1
-  thursdays = 1
-  fridays = 1
-  saturdays = 1
-  sundays = 1
-
-  mon_delta = mon1 - date
-  tue_delta = tue1 - date
-  wed_delta = wed1 - date
-  thu_delta = thu1 - date
-  fri_delta = fri1 - date
-  sat_delta = sat1 - date
-  sun_delta = sun1 - date
-
-  if mon_delta/7 == range(1, 750000, 1):
+  weekday = date.isoweekday()
+  if weekday == 1:
     mondays += 1
-  if tue_delta/7 == range(1,750000, 1):
+  if weekday == 2:
     tuesdays += 1
-  if wed_delta/7 == range(1, 750000, 1):
+  if weekday == 3:
     wednesdays += 1
-  if thu_delta/7 == range(1,750000, 1):
+  if weekday == 4:
     thursdays += 1
-  if fri_delta/7 == range(1,750000, 1):
+  if weekday == 5:
     fridays += 1
-  if sat_delta/7 == range(1,750000, 1):
+  if weekday == 6:
     saturdays += 1
-  if sun_delta/7 == range(1,750000, 1):
+  if weekday == 7:
     sundays += 1
 
 print("There were ", last, "total requests made in the last year.")
